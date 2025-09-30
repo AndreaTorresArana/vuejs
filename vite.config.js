9f2a88dbe8fea3634b014ab0a5767e36fbac0d36
AndreaTorresArana <145413509+AndreaTorresArana@users.noreply.github.com>
2025-09-30 11:56:54 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // Esta línea es la importante
-  base: '/vuejs/' 
+  // Esta es la línea MÁS IMPORTANTE. 
+  // Le dice a Vite que tu página vivirá en el subdirectorio /vuejs/
+  base: '/vuejs/'
 })
